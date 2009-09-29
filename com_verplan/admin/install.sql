936d8eec1eece7a4ee8dbc1f8bd73e0ea6aa4fdb
dominikmoritz <dominikmoritz@b057ebf6-a139-11de-a68b-fb984cb8e5f7>
2009-09-29 18:52:49 +0000

    Version: 0.0.11; updated sql with new tables: head, plan

--- com_verplan/admin/install.sql
+++ com_verplan/admin/install.sql
@@ -14,5 +14,27 @@ INSERT INTO `#__com_verplan_settings` (
 `value` ,
 `editable`
 )
-VALUES (NULL , 'max_file_size', '2000', '1'),
-(NULL , 'allowed_filetypes', 'html,htm,gif,jpg,png,pdf', '1');
+VALUES (NULL , 'max_file_size', '2097152', '1'),
+(NULL , 'allowed_filetypes', 'html,htm,gif,jpg,png,pdf,doc,odf,xls', '1');
+
+
+
+DROP TABLE IF EXISTS `#__com_verplan_plan`;
+
+CREATE TABLE `#__com_verplan_plan` (
+`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'primary key',
+`timestamp` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'creation time',
+`date` TIMESTAMP NOT NULL,
+`stand` TIMESTAMP NOT NULL
+) ENGINE = MYISAM CHARACTER SET utf8 COLLATE utf8_bin;
+
+
+DROP TABLE IF EXISTS `#__com_verplan_head`;
+
+CREATE TABLE `#__com_verplan_head` (
+`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+`name` TEXT NOT NULL ,
+`show` BOOL NOT NULL DEFAULT '0',
+`sort_order` INT NOT NULL ,
+`alternative_name` INT NOT NULL
+) ENGINE = MYISAM CHARACTER SET utf8 COLLATE utf8_bin;
--- com_verplan/admin/uninstall.sql
+++ com_verplan/admin/uninstall.sql
@@ -1,1 +1,3 @@
 DROP TABLE IF EXISTS `#__com_verplan_settings`;
+DROP TABLE IF EXISTS `#__com_verplan_plan`;
+DROP TABLE IF EXISTS `#__com_verplan_head`;
