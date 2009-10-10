1c22e2bfa3b6e07cb392b789070c60c88b36d211
dominikmoritz <dominikmoritz@b057ebf6-a139-11de-a68b-fb984cb8e5f7>
2009-10-10 20:08:50 +0000

    Version: 0.0.17; jQuery Plugins

---
+++ com_verplan/site/includes/js/jquery.table.js
@@ -0,0 +1,84 @@
+/**
+ * initialisiert die tabelle
+ * @return
+ */
+function table_init(){
+	table_update();
+	
+	/*
+	 * tablesorter plugin
+	 * http://tablesorter.com/docs/
+	 */
+	jQuery('#jquerytable').tablesorter({
+		dateFormat:'de',
+		decimal: ',',
+		debug:false,
+		sortMultiSortKey:'ctrlKey',
+		textExtraction:'complex',
+		//zebra
+		widgets: ['zebra']
+	});	
+
+    /*
+     * wenn neu sortiert wird, dann wird auch der indikator gestartet
+     * assign the sortStart event 
+     */
+	jQuery("#jquerytable").bind("sortStart",function() { 
+		jQuery('div#loading').fadeIn(200); 
+    }).bind("sortEnd",function() { 
+    	jQuery('div#loading').fadeOut(1000);
+    }); 
+	
+	
+	/*
+	 * filter
+	 * http://plugins.jquery.com/project/ColumnFilters
+	 * http://www.tomcoote.co.uk/jQueryColumnFilters.aspx
+	 */
+	jQuery('#jquerytable').columnFilters({
+		alternateRowClassNames:['even','odd'],
+		underline:false,
+		caseSensitive:false
+	});
+	
+}
+
+/**
+ * update/repaint nach ajax update
+ * @return
+ */
+function table_update() {
+	/*
+	 * zebramuster
+	 */
+	jQuery("#jquerytablebody tr").removeClass("even");
+	jQuery("#jquerytablebody tr").removeClass("odd");
+	jQuery("#jquerytable tbody tr:even").addClass('even');
+	jQuery("#jquerytable tbody tr:odd").addClass('odd');
+	
+	/*
+	 * colorize farben
+	 * http://plugins.jquery.com/project/Colorize
+	 */
+	jQuery('#jquerytable tbody').colorize({
+		altColor: 'none',
+		bgColor: 'none',
+		hover: 'row', 
+		click: 'row', 
+		hiliteClass: 'marked', 
+		hoverClass: 'hover'
+	});
+	
+	/*
+	 * tablesorter update
+	 */
+	jQuery('#jquerytable').trigger("update");
+	
+	/*
+	 * highlight effect für tr
+	 */
+	jQuery("#jquerytable tbody tr").click(function(){
+		jQuery(this).effect('highlight','slow');
+	});	
+	/**/
+}
--- com_verplan/site/views/verplan/tmpl/default.php
+++ com_verplan/site/views/verplan/tmpl/default.php
@@ -13,28 +13,44 @@ defined('_JEXEC') or die('=;)');
 
 $document =& JFactory::getDocument();
 
+/*
+ * css
+ */
+$document->addStylesheet('components/com_verplan/includes/css/general.css');
+
+
+/*
+ * scripts
+ */
+
 //jQuery hinzufügen
 $document->addScript('http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js');
 $document->addScript('http://ajax.googleapis.com/ajax/libs/jqueryui/1.7/jquery-ui.min.js');
 
-//css
-$document->addStylesheet('components/com_verplan/includes/css/general.css');
-
 //no conflict mode für jQuery (http://docs.jquery.com/Using_jQuery_with_Other_Libraries)
 $document->addCustomTag( '<script type="text/javascript">var jQuery = jQuery.noConflict();</script>' );
 
-//scripts
+//eigene scripts
 $document->addScript($this->baseurl.'/components/com_verplan/includes/js/ajax.js');
+$document->addScript($this->baseurl.'/components/com_verplan/includes/js/jquery.table.js');
+
+//plugins
+$document->addScript($this->baseurl.'/components/com_verplan/includes/js/plugins/jquery.tablesorter.js');
+$document->addScript($this->baseurl.'/components/com_verplan/includes/js/plugins/jquery.columnfilters.js');
+$document->addScript($this->baseurl.'/components/com_verplan/includes/js/plugins/jquery.colorize-2.0.0.js');
+
+
 
 //$document->addScript('http://www.google.com/jsapi');
 //$document->addScript($this->baseurl.'/components/com_verplan/includes/js/googletable.js');
 //$document->addCustomTag( "<script type=\"text/javascript\">google.load('visualization', '1', {packages: ['table']});</script>" );
+//$document->addScript($this->baseurl.'/components/com_verplan/includes/dataTables-1.5/media/js/jQuery.dataTables.js');
 ?>
 
 <!-- -->
-<form name="upload" method="post" enctype="multipart/form-data"	action="">
+<form id="verplan_form" name="upload" method="post" enctype="multipart/form-data"	action="">
 
-	<div id="selectrahmen">
+	<div id="selectrahmen" class="corner-all">
 	<label for="select_date"></label>  
 	<select size="1" id="select_date" name="date">
 		<?php 
@@ -75,27 +91,29 @@ $document->addScript($this->baseurl.'/components/com_verplan/includes/js/ajax.js
 	</div>
 	
 	<!-- nur den neuesten stand --> 
-	<input type="hidden" name="stand" value="newest" /> 
+	<input type="hidden" name="stand" value="latest" /> 
+	<input type="hidden" name="options" value="" />
 	
 	<!-- <input type="submit" name="submit" class="submitbutton" value="Anzeigen" />-->
-
-	<!-- anzeige ohne template (praktisch für ajax) --> 
-	<!--<input type="hidden" name="tmpl" value="component" />-->
 
 	<!-- damit die Komponente wieder aufgerufen wird --> 
 	<input type="hidden" name="option" value="com_verplan" /> 
 	<input type="hidden" name="task" value="" />
-	<!-- direkte anzeige der JSON daten--> 
 	<input type="hidden" name="format" value="js" /> 
 	<input type="hidden" name="boxchecked" value="0" /> 
-	<!-- richtiger Controller --> 
 	<input type="hidden" name="controller" value="" /> 
 	<!-- die user ID --> 
 	<input	type="hidden" name="id" value="<?php echo $this->user->id; ?>" />
 </form>
 
-<div id="ajaxtable">
-<table id="ajaxtable_table">
+<noscript>
+	<div class="corner-all error">
+	<?php echo $this->nojs;?>	
+	</div>
+</noscript>
+
+<div id="ajaxdiv">
+<table id="jquerytable" class="display">
 	<colgroup>
 	<?php
 	$array = $this->verplanArray;
