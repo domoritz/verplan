755873fd7816b3a0b74def8e5f998f9a13e749b3
dominikmoritz <dominikmoritz@b057ebf6-a139-11de-a68b-fb984cb8e5f7>
2009-10-20 09:36:17 +0000

    Version: 0.1.0; admin headings

--- trunk/com_verplan/admin/views/verplan/tmpl/default.php
+++ trunk/com_verplan/admin/views/verplan/tmpl/default.php
@@ -43,7 +43,7 @@ $document->addScript('components/com_verplan/includes/js/hide_admin.js');
 	<!-- anzeige ohne template (praktisch für ajax) -->
 	<!--<input type="hidden" name="tmpl" value="component" />-->
 		
-	<!-- sollen fehler in den regulaeren ausdruechen ignoriert werden? (empfohlen) --> 
+	<!-- sollen fehler in den regulaeren ausdrücken ignoriert werden? (empfohlen) --> 
 	<input type="hidden" name="ignore" value="true" /> 
 
 	<!-- damit die Komponente wieder aufgerufen wird --> 
