c63be10e932b2294173f453a5dcc01bd94ea47e1
dominikmoritz <dominikmoritz@b057ebf6-a139-11de-a68b-fb984cb8e5f7>
2009-10-25 19:06:55 +0000

    Version: 0.1.7; main controller: send

--- com_verplan/admin/controllers/send.php
+++ com_verplan/admin/controllers/send.php
@@ -34,16 +34,17 @@ class VerplanControllerSend extends verplanController
 	}
 
 	/**
-	 * 
+	 *
 	 * @return void
 	 */
 	function send() {
-	    //var_dump($this->getTasks());
-	    
+		//var_dump($this->getTasks());
+	  
 		//holt die dateiinfos
 		$file = JRequest::getVar('file', null, 'files', 'array');
+		//debug
 		//var_dump($file);
-		
+
 		//Dateiinhalt der plandatei aus temp laden
 		$FileHandle = fopen($file[tmp_name], "r" ) ;
 		$n = $file[size];
@@ -52,20 +53,48 @@ class VerplanControllerSend extends verplanController
 
 		//falls es sich um eine datei handelt, die in die DB eigelesen werden kann
 		if (strpos($inhalt, "Untis")) {
-			echo "upload";
 			// upload controller laden
 			$name = 'upload';
 			require_once(JPATH_COMPONENT.DS.'controllers'.DS.$name.'.php');
-			
+				
 			$controllerName = verplanController.ucfirst($name);
-			$controller = new $controllerName();		
+			$controller = new $controllerName();
+				
+			//rückgabewert der methode upload aus upload
+			$dest = $controller->execute('upload');
 			
-	    	//$controller->execute('upload');
+			//debug
+			var_dump($dest);
+				
+			//nun kann die datei geparst werden
+			$data = $controller->parse_file_to_array($dest);
+			
+			//debug
+			var_dump($data);
+			
+			
+			//und dann werden die daten noch gespeichert
+			/*
+			 * an dieser stelle wird das model data aufgerufen und die methode
+			 * store mit dem array des vertretungsplanes als uebergabewert aufgerufen
+			 *
+			 * das array enthält alle daten, des planes inklusive einer id für datum und stand
+			 */	
+			$model = $this->getModel('data');
+			//$model->store($data);
+
+			if (!JERROR::getError()) {
+				//Erfolg melden
+				//zu bebuggzwecken kann man dies auskommentieren und kann sich dann den ablauf ansehen
+				$msg = 'Parsen und Senden erfolgreich';
+				//$this->setRedirect( 'index.php?option=com_verplan', $msg );
+			}
+
 		} else {
-			$msg = 'Datei kann nicht eingelesen werden';
+			$msg = "Datei eingestellt, ohne DB";
 			$this->setRedirect( 'index.php?option=com_verplan', $msg );
 		}
-		
-		
+
+
 	}
 }
--- com_verplan/admin/controllers/upload.php
+++ com_verplan/admin/controllers/upload.php
@@ -35,7 +35,7 @@ class VerplanControllerUpload extends verplanController
 	}
 
 	/**
-	 * lädt den Vertreutungsplan hoch
+	 * lädt den die vertretungsplandatei hoch
 	 * @return void
 	 */
 	function upload() {
@@ -70,20 +70,11 @@ class VerplanControllerUpload extends verplanController
 		if ( in_array(strtolower(JFile::getExt($filename)),$allowed_filetypes) OR false) {
 			if ($filesize <= $settings['max_file_size']) {
 				if (JFile::upload($src, $dest) ) {
-					//Redirect to a page of your choice
-					if (strtolower(JFile::getExt($filename)) == "htm" || strtolower(JFile::getExt($filename)) == "html"){
-						$this->parse_file_to_array($dest);
-						//falls es keine fehler gab
-						if (!JERROR::getError()) {
-							//Erfolg melden
-							//zu bebuggzwecken kann man dies auskommentieren und kann sich dann den ablauf ansehen
-							$msg = 'Parsen und Senden erfolgreich';
-							$this->setRedirect( 'index.php?option=com_verplan', $msg );
-						}
-					} else {
-						$msg = 'Upload erfolgreich, Redirect auf die Datei und nicht die richtige Datenbankanzeige';
-						$this->setRedirect( 'index.php?option=com_verplan', $msg );
-					}
+					/*
+					 * Erfolg melden, upload erfolgreich
+					 * es wird true an den controller sent zurückgegeben, welcher nun weiter verfährt
+					 */
+					return ($dest);
 				} else {
 					//Redirect and throw an error message
 					$msg = "Upload nicht erfolgreich, Fehler beim Hochladen";
@@ -144,183 +135,171 @@ class VerplanControllerUpload extends verplanController
 		$inhalt = fread( $FileHandle , $n ) ;
 		fclose( $FileHandle ) ;
 
-		//falls es sich um eine gp-untis datei handelt
-		if (true OR strpos($inhalt, "gp-Untis")) {
-			//debug
-			echo "<pre>";
+		//extractor, parser
+		/*
+		* extractor einbinden
+		* der extractor und parser ist ein externes programm, welches sich in
+		* admin/includes/js-extractor_0.1.1 befindet
+		*/
+		require_once("components/com_verplan/includes/js-extractor_0.1.1/library/JS/Extractor.php");
+		//neue instanz des Extractors
+		$extractor = new JS_Extractor($inhalt);
+		$body = $extractor->query("body")->item(0);
+		$table = $body->query("//table")->item(0);
 
-			//extractor, parser
-			/*
-			* extractor einbinden
-			* der extractor und parser ist ein externes programm, welches sich in
-			* admin/includes/js-extractor_0.1.1 befindet
-			*/
-			require_once("components/com_verplan/includes/js-extractor_0.1.1/library/JS/Extractor.php");
-			//neue instanz des Extractors
-			$extractor = new JS_Extractor($inhalt);
-			$body = $extractor->query("body")->item(0);
-			$table = $body->query("//table")->item(0);
+		/*STAND*/
 
-			/*STAND*/
+		/*
+		 * alternative Lösungen
+		 */
+		//0. stand aus tabellenzelle
+		//$data = $table->extract(array(".//tr", "th|td"));
+		//$standstring = $data['2']['4'];
 
-			/*
-			 * alternative Lösungen
-			 */
-			//0. stand aus tabellenzelle
-			//$data = $table->extract(array(".//tr", "th|td"));
-			//$standstring = $data['2']['4'];
-
-			//1. stand mit dom über nummer der zelle
-			//$td = $extractor->query("//td");
-			//$standstring = $td->item(12)->nodeValue;
-			/*
-			* ende alternative Lösungen
-			*/
+		//1. stand mit dom über nummer der zelle
+		//$td = $extractor->query("//td");
+		//$standstring = $td->item(12)->nodeValue;
+		/*
+		* ende alternative Lösungen
+		*/
 
 
-			// 2. stand ueber regulaeren ausdruck
-			/*
-			* regulaerer ausdruck: Stand: dann zeichen dann Uhrzeit mit :,
-			* U=ungreedy
-			* */
-			$pattern = '/Stand:.*:[0-5][0-9]/U';
-			if(preg_match_all($pattern,$inhalt,$matches)){
-				//falls es mehr als zwei treffer gibt, sollte es eine fehlermeldung geben
-				if($matches[1] && !$ignore){
-					//debug
-					foreach ($matches as $y => $match) {
-						var_dump($match);
-					}
-					$msg = "Fehler beim Parsen (stand, regexp, zu viele entsprechungen, line: ".__LINE__.")";
-					$this->setRedirect( 'index.php?option=com_verplan', $msg );
-				} else {
-					foreach ($matches as $y => $match) {
-						//debug
-						//var_dump($match);
-						$standstring = $match[0];
-					}
-
+		// 2. stand ueber regulaeren ausdruck
+		/*
+		* regulaerer ausdruck: Stand: dann zeichen dann Uhrzeit mit :,
+		* U=ungreedy
+		* */
+		$pattern = '/Stand:.*:[0-5][0-9]/U';
+		if(preg_match_all($pattern,$inhalt,$matches)){
+			//falls es mehr als zwei treffer gibt, sollte es eine fehlermeldung geben
+			if($matches[1] && !$ignore){
+				//debug
+				foreach ($matches as $y => $match) {
+					var_dump($match);
 				}
-			} else {
-				$msg = "Fehler beim Parsen (stand, regexp, nichts gefunden, line: ".__LINE__.")";
+				$msg = "Fehler beim Parsen (stand, regexp, zu viele entsprechungen, line: ".__LINE__.")";
 				$this->setRedirect( 'index.php?option=com_verplan', $msg );
+			} else {
+				foreach ($matches as $y => $match) {
+					//debug
+					//var_dump($match);
+					$standstring = $match[0];
+				}
+
 			}
-
-			//strip "Stand:"
-			$standstring = substr($standstring,0);
-
-			//Datumsformat parsen !!ACHTUNG, benoetigt >= PHP 5.3!!
-			$stand_array = date_parse_from_format("d.m.Y H:i", $standstring);
-			//var_dump($stand_array);
-			//array in unix timestamp wandeln
-			$stand = mktime($stand_array[hour],$stand_array[minute],$stand_array[second],$stand_array[month],$stand_array[day],$stand_array[year]);
-
-			//debug
-			setlocale(LC_TIME, "de_DE");
-			$format="Stand: %A %d.%m.%Y %H:%M";
-			$strf=strftime($format,$stand);
-			echo "$strf <br>";
-			//echo $standstring;
-
-
-			/*DATUM*/
-			/*foreach ($extractor->query("//div[@class='mon_title']") as $link) {
-			 $datestring = $link->nodeValue."<br>";
-			 }*/
-
-			//laedt alle passenden div mit class und class="mon_title"
-			$div = $extractor->query("//div[@class='mon_title']");
-			$datestring = $div->item(0)->nodeValue;
-
-			//Datumsformat parsen
-			$date_array = date_parse_from_format("j.n.Y w", $datestring);
-			//var_dump($date_array);
-			//array in unix timestamp wandeln
-			$date = mktime($date_array[hour],$date_array[minute],$date_array[second],$date_array[month],$date_array[day],$date_array[year]);
-
-			//debug
-			setlocale(LC_TIME, "de_DE");
-			$format="Date: %A %d.%m.%Y %H:%M";
-			$strf=strftime($format,$date);
-			echo "$strf <br>";
-
-
-			/*TABELLE*/
-			//alle tabellen und trennt dann nach tr oder td
-
-			//$table = $body->query("//table")->item(2);
-			//$data = $table->extract(array(".//tr", "th|td"));
-
-			$table = $extractor->query("//table[@class='mon_list']")->item(0);
-			$data = $table->extract(array(".//tr", "th|td"));
-
-			//debug
-			//var_dump($data);
-
-			//zaehlt die anzahl der spalten (count von subarray), hier nicht verwendet
-			//$columns = count($data[0]);
-			
-			$date = date( 'Y-m-d H:i:s', $date );
-			$stand = date( 'Y-m-d H:i:s', $stand );
-			
-			//array für uploads tabelle
-			$upload_arr[Geltungsdatum] = $date; //geltungsdatum
-			$upload_arr[Stand] = $stand; //stand
-			$upload_arr[type] = 'db'; //typ, hier datenbank
-			$upload_arr[url] = $file; //url zur hochgeladenen datei (hier parameter)
-			
-			
-			/*
-			 * hier wird das model aufgerufen und die tabelle mit den uploads wird beschrieben. 
-			 * darin sind z.b stand und datum vermerkt und über eine id erreichbar.
-			 * diese id taucht dann wieder in der plantabelle auf -> normalisierung
-			 */
-			$model = $this->getModel('data');
-			$id = $model->log_in_uploads($upload_arr);
-			
-			
-			//array mit plandaten zum uebergeben vorbereiten (datum und stand anhaengen)
-			//tabellenkopf
-			$data[0][] = "id_upload";
-
-			/*
-			 * umwandeln der timestamps in mysql timestamps
-			 * kann einfach entfernt werden, dann wird nicht umgewandelt
-			 *
-			 * Timestamps
-			 *
-			 * PHP -> MySQL
-			 * $date = date( 'Y-m-d H:i:s', $date );
-			 *
-			 * MySQL -> PHP
-			 * $date = strtotime($date);
-			 *
-			 */
-
-			//tabellenzellen
-			for ($i = 1; $i < count($data); $i++) {
-				$data[$i][] = $id;
-			}
-
-			//debug
-			//var_dump($data);
-
-
-			/*
-			 * an dieser stelle wird das model data aufgerufen und die methode
-			 * store mit dem array des vertretungsplanes als uebergabewert aufgerufen
-			 *
-			 * das array enthält alle daten, des planes inklusive einer id für datum und stand
-			 */			
-			$model->store($data);
-
-			//debug
-			echo "</pre>";
-
 		} else {
-			$msg = "Datei eingestellt, ohne DB";
+			$msg = "Fehler beim Parsen (stand, regexp, nichts gefunden, line: ".__LINE__.")";
 			$this->setRedirect( 'index.php?option=com_verplan', $msg );
 		}
+
+		//strip "Stand:"
+		$standstring = substr($standstring,0);
+
+		//Datumsformat parsen !!ACHTUNG, benoetigt >= PHP 5.3!!
+		$stand_array = date_parse_from_format("d.m.Y H:i", $standstring);
+		//var_dump($stand_array);
+		//array in unix timestamp wandeln
+		$stand = mktime($stand_array[hour],$stand_array[minute],$stand_array[second],$stand_array[month],$stand_array[day],$stand_array[year]);
+
+		//debug
+		setlocale(LC_TIME, "de_DE");
+		$format="Stand: %A %d.%m.%Y %H:%M";
+		$strf=strftime($format,$stand);
+		echo "$strf <br>";
+		//echo $standstring;
+
+
+		/*DATUM*/
+		/*foreach ($extractor->query("//div[@class='mon_title']") as $link) {
+		 $datestring = $link->nodeValue."<br>";
+		 }*/
+
+		//laedt alle passenden div mit class und class="mon_title"
+		$div = $extractor->query("//div[@class='mon_title']");
+		$datestring = $div->item(0)->nodeValue;
+
+		//Datumsformat parsen
+		$date_array = date_parse_from_format("j.n.Y w", $datestring);
+		//var_dump($date_array);
+		//array in unix timestamp wandeln
+		$date = mktime($date_array[hour],$date_array[minute],$date_array[second],$date_array[month],$date_array[day],$date_array[year]);
+
+		//debug
+		setlocale(LC_TIME, "de_DE");
+		$format="Date: %A %d.%m.%Y %H:%M";
+		$strf=strftime($format,$date);
+		echo "$strf <br>";
+
+
+		/*TABELLE*/
+		//alle tabellen und trennt dann nach tr oder td
+
+		//$table = $body->query("//table")->item(2);
+		//$data = $table->extract(array(".//tr", "th|td"));
+
+		$table = $extractor->query("//table[@class='mon_list']")->item(0);
+		$data = $table->extract(array(".//tr", "th|td"));
+
+		//debug
+		//var_dump($data);
+
+		//zaehlt die anzahl der spalten (count von subarray), hier nicht verwendet
+		//$columns = count($data[0]);
+			
+		$date = date( 'Y-m-d H:i:s', $date );
+		$stand = date( 'Y-m-d H:i:s', $stand );
+			
+		//array für uploads tabelle
+		$upload_arr[Geltungsdatum] = $date; //geltungsdatum
+		$upload_arr[Stand] = $stand; //stand
+		$upload_arr[type] = 'db'; //typ, hier datenbank
+		$upload_arr[url] = $file; //url zur hochgeladenen datei (hier parameter)
+			
+			
+		/*
+		 * hier wird das model aufgerufen und die tabelle mit den uploads wird beschrieben.
+		 * darin sind z.b stand und datum vermerkt und über eine id erreichbar.
+		 * diese id taucht dann wieder in der plantabelle auf -> normalisierung
+		 */
+		$model = $this->getModel('data');
+		$id = $model->log_in_uploads($upload_arr);
+			
+			
+		//array mit plandaten zum uebergeben vorbereiten (datum und stand anhaengen)
+		//tabellenkopf
+		$data[0][] = "id_upload";
+
+		/*
+		 * umwandeln der timestamps in mysql timestamps
+		 * kann einfach entfernt werden, dann wird nicht umgewandelt
+		 *
+		 * Timestamps
+		 *
+		 * PHP -> MySQL
+		 * $date = date( 'Y-m-d H:i:s', $date );
+		 *
+		 * MySQL -> PHP
+		 * $date = strtotime($date);
+		 *
+		 */
+
+		//tabellenzellen
+		for ($i = 1; $i < count($data); $i++) {
+			$data[$i][] = $id;
+		}
+
+		//debug
+		//var_dump($data);
+
+
+		/*
+		 * an dieser stelle wird das model data aufgerufen und die methode
+		 * store mit dem array des vertretungsplanes als uebergabewert aufgerufen
+		 *
+		 * das array enthält alle daten, des planes inklusive einer id für datum und stand
+		 */
+		//$model->store($data);
+		return $data;
 	}
 
 }
