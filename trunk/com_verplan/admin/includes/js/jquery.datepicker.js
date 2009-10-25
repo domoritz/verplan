d285fb3c400ee89bfbfa91597b55e1409596a73a
dominikmoritz <dominikmoritz@b057ebf6-a139-11de-a68b-fb984cb8e5f7>
2009-10-25 17:46:22 +0000

    Version: 0.1.7; backend added time chooser

--- trunk/com_verplan/admin/includes/js/jquery.datepicker.js
+++ trunk/com_verplan/admin/includes/js/jquery.datepicker.js
@@ -10,8 +10,19 @@
 jQuery(function() {
 	
 	//datepicker
+	
+	jQuery('#datepicker_date').datepicker({		
+		showOn: 'button', 
+		numberOfMonths: 2,
+		showButtonPanel: true,
+		dateFormat: 'yy-mm-dd',
+		buttonImage: 'components/com_verplan/includes/images/office_calendar.png',
+		buttonText: 'Geltungsdatum wählen',
+		buttonImageOnly: true,
+		changeMonth: false
+	});  
 
-	jQuery("#datepicker_date").datepicker({
+	/*jQuery("#datepicker_date").datepicker({
 		showOn: 'button', 
 		numberOfMonths: 2,
 		showButtonPanel: true,
@@ -20,9 +31,15 @@ jQuery(function() {
 		buttonText: 'Geltungsdatum wählen',
 		buttonImageOnly: true,
 		changeMonth: false
-	},jQuery.datepicker.regional['de']);
+	},jQuery.datepicker.regional['de']);*/
 	
-	jQuery("#datepicker_stand").datepicker({
+	
+	//mit unter
+	/*
+	jQuery('#datepicker_stand').datetimepicker({
+		timeFormat: ' hh-ii-ss',
+		
+		
 		showOn: 'button', 
 		numberOfMonths: 1,
 		showButtonPanel: true,
@@ -31,6 +48,40 @@ jQuery(function() {
 		buttonText: 'Stand wählen',
 		buttonImageOnly: true,
 		changeMonth: false
-
 	},jQuery.datepicker.regional['de']);
+	*/
+	
+	jQuery('#datepicker_stand').datepicker({
+		duration: '',
+        showTime: false,
+        constrainInput: false,
+        stepMinutes: 10,
+        stepHours: 10,
+        altTimeField: '',
+        time24h: true,
+		
+		
+		showOn: 'button', 
+		numberOfMonths: 1,
+		showButtonPanel: true,
+		dateFormat: 'yy-mm-dd',
+		buttonImage: 'components/com_verplan/includes/images/office_calendar.png', 
+		buttonText: 'Stand wählen',
+		buttonImageOnly: true,
+		changeMonth: false
+	},jQuery.datepicker.regional['de']);
+	
+	/*
+	jQuery('#datepicker_stand_time').kmTimepicker({
+		time24h: true
+	});
+	*/
+	
+	/*http://code.google.com/p/jquery-utils/wiki/UiTimepickr*/
+	$('#datepicker_stand_time').timepickr({
+		convention: 24,
+		val: '00:00',
+		format24: '{h:02.d}:{m:02.d}' 
+	}); 
+	
 });
--- trunk/com_verplan/admin/views/verplan/tmpl/default.php
+++ trunk/com_verplan/admin/views/verplan/tmpl/default.php
@@ -14,7 +14,7 @@ defined('_JEXEC') or die('=;)');
 $document =& JFactory::getDocument();
 
 //jQuery support
-$document->addScript('http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js');
+$document->addScript('http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js');
 $document->addScript('http://ajax.googleapis.com/ajax/libs/jqueryui/1.7/jquery-ui.min.js');
 $document->addScript('http://jqueryui.com/ui/i18n/ui.datepicker-de.js');
 
@@ -23,13 +23,23 @@ $document->addStylesheet('http://ajax.googleapis.com/ajax/libs/jqueryui/1.7.2/th
 $document->addStylesheet('components/com_verplan/includes/css/general.css');
 
 //no conflict mode für jQuery (http://docs.jquery.com/Using_jQuery_with_Other_Libraries)
-$document->addCustomTag( '<script type="text/javascript">var jQuery = jQuery.noConflict();</script>' );
+//$document->addCustomTag( '<script type="text/javascript">var jQuery = jQuery.noConflict();</script>' );
 
 //Javascript
 $document->addScript('components/com_verplan/includes/js/hide_admin.js');
 $document->addScript('components/com_verplan/includes/js/jquery.datepicker.js');
 
+//Plugins
+/*http://milesich.com/timepicker/*/
+$document->addScript('components/com_verplan/includes/js/plugins/timepicker.js');
+/*http://stackoverflow.com/questions/1028409/jquery-datepicker-and-timepicker-for-same-input-field-to-popup-one-after-another*/
+//$document->addScript('components/com_verplan/includes/js/plugins/ui.datetimepicker.js');
+/*http://haineault.com/media/jquery/ui-timepickr/page/*/
+//$document->addScript('components/com_verplan/includes/js/plugins/km.timepicker.js');
 
+/*http://haineault.com/media/jquery/ui-timepickr/page/*/
+$document->addScript('components/com_verplan/includes/js/plugins/jquery.timepickr.js');
+$document->addStylesheet('components/com_verplan/includes/css/ui.timepickr.css');
 ?>
 
 <h1>Vertretungsplan</h1>
@@ -54,14 +64,18 @@ $document->addScript('components/com_verplan/includes/js/jquery.datepicker.js');
 			<tr>
 				<td class="key"><label for="date">Geltungsdatum</label></td>
 				<td>
-					<input size="40" type="date" id="datepicker_date" name="date" class="inputbox" /><br>
+					<input size="40" type="text" id="datepicker_date" name="date" class="inputbox" />
 				</td>
 			</tr>
 			<tr>
 				<td class="key"><label for="stand">Stand</label></td>
 				<td>
-					<input size="40" type="stand" id="datepicker_stand" name="stand" class="inputbox" /><br>
+					<input size="40" type="text" id="datepicker_stand" name="stand" />
+					<input size="40" type="text" id="datepicker_stand_time" name="stand" />
 				</td>
+				<!-- <td>
+					<input size="40" type="stand_time" id="datepicker_stand_time" name="stand" class="inputbox" /><br>
+				</td> -->
 			</tr>
 		</tbody>
 	</table>
