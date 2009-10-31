08de7c3f31db4c2b32ee3e2220c2a14da6281321
dominikmoritz <dominikmoritz@b057ebf6-a139-11de-a68b-fb984cb8e5f7>
2009-10-31 10:12:44 +0000

    Version: 0.1.11; js for admin column, template, new view for ajax

---
+++ trunk/com_verplan/admin/views/verplan/view.js.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @version		$Id$
+ * @package		verplan
+ * @author		Dominik Moritz {@link http://www.dmoritz.bplaced.net}
+ * @link		http://code.google.com/p/verplan/
+ * @license		GNU/GPL
+ * @author      Created on 31-Oct-2009
+ */
+
+//-- No direct access
+defined('_JEXEC') or die('=;)');
+
+jimport( 'joomla.application.component.view');
+
+/**
+ * HTML View class for the verplan Component
+ *
+ * @package    verplan
+ */
+class verplanViewVerplan extends JView
+{
+	function display($tpl = null)
+	{
+				
+
+		parent::display($tpl);
+	}// function
+}// class
