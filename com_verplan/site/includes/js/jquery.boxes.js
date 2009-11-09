cb98a29d0e9b7c4f02ec2054f8d102d91bab9218
dominikmoritz <dominikmoritz@b057ebf6-a139-11de-a68b-fb984cb8e5f7>
2009-11-09 15:30:37 +0000

    Version: 0.1.16; better js

---
+++ com_verplan/site/includes/js/jquery.boxes.js
@@ -0,0 +1,13 @@
+/**
+ * @version		$Id$
+ * @package		verplan
+ * @author		Dominik Moritz {@link http://www.dmoritz.bplaced.net}
+ * @link		http://code.google.com/p/verplan/
+ * @license		GNU/GPL
+ * @author      Created on 07-Nov-2009
+ */
+
+jQuery(document).ready(function(){
+	//http://www.no-margin-for-errors.com/projects/prettyPhoto-jquery-lightbox-clone/#themes
+	jQuery("a[rel^='prettyPhoto']").prettyPhoto();
+});
