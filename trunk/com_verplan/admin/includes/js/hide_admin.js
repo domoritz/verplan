ebdee51ffd1b6512494565d3c583657a8811da11
dominikmoritz <dominikmoritz@b057ebf6-a139-11de-a68b-fb984cb8e5f7>
2009-12-19 13:50:18 +0000

    Version: 0.9.7; fixed hide and show bug in admin (hash was wrong if you closed a panel)

--- trunk/com_verplan/admin/includes/js/hide_admin.js
+++ trunk/com_verplan/admin/includes/js/hide_admin.js
@@ -7,24 +7,33 @@
  * @author      Created on 2-Oct-2009
  */
 jQuery(document).ready(function(){
-	
+
 	jQuery("#accordion h3 a").click(function(event){
-		  window.location.hash=this.hash;
-	 });
-	
+		window.location.hash=this.hash;
+	});
+
 	//alle divs verstecken, die für optionen sind
 	jQuery('a.expander').next('div').hide('');
-	
+
 	//divs öffnen und schließen
-	jQuery('.expander').click(function() {		
-		
+	jQuery('.expander').click(function() {	
+
+		//hash setzen, beim öffnen soll er gesetzt bleiben (aus link), beim schließen entfernt
+		if (jQuery(this).attr('class').search('minus') >= 0) {
+			window.location.hash = '#';
+		} else {
+			window.location.hash = jQuery(this).attr('id');
+		}
+
 		//alle anderen , die offen sind schließen		
 		jQuery(".minus").not(this).addClass('plus').removeClass('minus').next('.verschwinder').toggle('blind','slow');
-		
-		//icon wechseln und dieses öffnen
+
+		//icon wechseln und dieses öffnen/schließen
 		jQuery(this).toggleClass('plus').toggleClass('minus').next('.verschwinder').toggle('blind','slow');		
+		
+		return false;
 	});
-	
+
 	var hash = window.location.hash;
 	//alert (hash);
 	if (hash) {
