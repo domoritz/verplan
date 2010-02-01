f6d6e419e52519e3bc3b580aa77f7b38f06420be
dominikmoritz <dominikmoritz@b057ebf6-a139-11de-a68b-fb984cb8e5f7>
2010-02-01 14:20:40 +0000

    Version: 0.9.22; new about

--- com_verplan/admin/views/about/tmpl/default.php
+++ com_verplan/admin/views/about/tmpl/default.php
@@ -19,8 +19,7 @@ $document =& JFactory::getDocument();
 $version = $this->settings[version]['default'];
 ?>
 
-<p>Komponente zum Einstellen des Vertretungsplanes auf die Website des
-MCG (http://www.marie-curie-gymnasium-dallgow.de/).</p>
+<p>Joomla Komponente zum Einstellen des Vertretungsplanes.</p>
 <ul>
 	<li>Version: <?php echo $version?></li>
 	<li>Lizenz: <a href="http://www.gnu.org/licenses/gpl-2.0.html">GNU/GPL</a></li>
@@ -28,15 +27,7 @@ MCG (http://www.marie-curie-gymnasium-dallgow.de/).</p>
 	2010</a></li>
 	<li>Code: <a href="http://code.google.com/p/verplan/">http://code.google.com/p/verplan/</a></li>
 </ul>
-<p>Verwendete Bibliotheken/Programme/Grafiken</p>
-<ul>
-	<li><a href="http://jquery.com/">jQuery</a></li>
-	<li><a href="http://plugins.jquery.com/">diverse jQuery Plugins</a></li>
-	<li><a href="http://jacksleight.com/old/code">JS Extractor</a></li>
-	<li><a href="http://www.oxygen-icons.org/">Icons aus dem Oxygen Package</a></li>
-	<li><a href="http://www.preloaders.net">Ajax Indikatoren</a></li>
-</ul>
-<p>Systemvoraussetzungen</p>
-<ul>
-	<li>PHP >= 5.3.0</li>
-</ul>
+
+<br>
+<a href="http://code.google.com/p/verplan/wiki/About">Mehr auf der Projektseite</a>
+<br>
