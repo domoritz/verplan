981e07093dbf03094a6b73e3a5260eaab92315b1
domoritz <domoritz@b057ebf6-a139-11de-a68b-fb984cb8e5f7>
2011-04-12 19:46:31 +0000

    new file for admin menus

--- trunk/com_verplan/admin/toolbar.verplan.php
+++ trunk/com_verplan/admin/toolbar.verplan.php
@@ -1,1 +1,13 @@
+<?php
+$controller   = JRequest::getCmd('controller', 'com_verplan');
 
+JHTML::_('behavior.switcher');
+
+// Load submenus
+$controllers = array('' => JText::_('Home'), '&view=columns' => JText::_('Spalten'), '&view=settings' => JText::_('Einstellungen'), '&view=about' => JText::_('About'), );
+
+foreach ($controllers as $key => $val) {
+   $active   = ($controller == $key);
+   JSubMenuHelper::addEntry($val, 'index.php?option=com_verplan'.$key, $active);
+}
+?>
